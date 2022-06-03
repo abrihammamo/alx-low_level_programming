c0784134e8e2e5d613862ad0480b1db48186c167
abriham mamao <abriham2016@gmail.com>
2022-06-03 03:47:37 -0700

    Q5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
+ *main - functin entry point
  *
- *
+ * Return: always return zero
  */
 int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
