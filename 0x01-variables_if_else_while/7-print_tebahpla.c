d788e5f0d85447493a5b08e2216849b3de60e27f
abriham mamao <abriham2016@gmail.com>
2022-06-03 07:17:07 -0700

    q 7 final

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+		putchar('\n');
+
+	return (0);
+}
