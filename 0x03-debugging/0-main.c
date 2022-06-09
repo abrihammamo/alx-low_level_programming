aa78b85636a754dbf566c1ad38c2181a2cd46aa0
abriham mamao <abriham2016@gmail.com>
2022-06-09 03:11:50 -0700

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: zero
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
