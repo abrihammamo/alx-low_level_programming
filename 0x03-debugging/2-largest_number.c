187d981c741699425b2067d69cfe1180df8c3280
abriham mamao <abriham2016@gmail.com>
2022-06-10 02:36:04 -0700

    2

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * largest_number - print the largest from three numbers
+ * @a: number 1
+ * @b: number 2
+ * @c: number 3
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	int l;
+
+	if (a >= b && a >= c)
+	{
+		l = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		l = b;
+	}
+	else if (c >= a && c >= b)
+	{
+		l = c;
+	}
+	return (1);
+}
